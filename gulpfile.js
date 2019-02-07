f8d97ba951507c7113735452d2dbffd3528c8d02
Matteo Baratin <mbaratin@alpenite.com>
2019-02-07 11:55:55 +0100

    added browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -7,18 +7,38 @@ const {
 const sass = require('gulp-sass');
 const include = require('gulp-html-tag-include');
 const babel = require('gulp-babel');
+const browsersync = require("browser-sync").create();
 const notify = require('gulp-notify');
+
+// BrowserSync
+function browserSync(done) {
+  browsersync.init({
+    server: {
+      baseDir: "./dist/"
+    },
+    port: 3000
+  });
+  done();
+}
+
+// BrowserSync Reload
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
+}
 
 function template() {
   return src('./src/template/**.html')
     .pipe(include())
     .pipe(dest('./dist'))
+    .pipe(browsersync.stream());
 }
 
 function style() {
   return src('./src/scss/**.scss')
     .pipe(sass())
     .pipe(dest('./dist/css'))
+    .pipe(browsersync.stream());
 }
 
 function js() {
@@ -27,6 +47,7 @@ function js() {
       presets: ['@babel/env']
     }))
     .pipe(dest('./dist/js'))
+    .pipe(browsersync.stream());
 }
 
 
@@ -35,7 +56,7 @@ function js() {
 function watchFiles() {
   watch("./src/template/**.html", template);
   watch("./src/scss/**.scss", style);
-  watch("./src/js/**.js", js)
+  watch("./src/js/**.js", js);
   src('./src/js/**.js')
   .pipe(notify('Gulp is Watching, cheer! 🍺'));
 }
@@ -43,5 +64,5 @@ function watchFiles() {
 exports.js = js;
 exports.template = template;
 exports.style = style;
-exports.default = parallel(template, style, js, watchFiles);
+exports.default = parallel(template, style, js, watchFiles, browserSync);
 exports.watch = watchFiles;
